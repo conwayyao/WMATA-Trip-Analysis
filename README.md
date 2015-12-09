1b4b41ac71416ae68d0856e8f86c7eecb59c5262
Conway Yao <theyaomeister@gmail.com>
2015-12-09 17:32:38 -0500

    Added trip separation code and basic analysis

--- README.md
+++ README.md
@@ -1,11 +1,47 @@
-# WMATA-Trip-Analysis
+# WMATA Trip Analysis
 
 
 Analysis of WMATA performance using Conway's own SmarTrip data
 
+.
 
+# Data Collection and Munging:
+
+## Excel steps
+
+1. Log into SmarTrip 
+2. Navigate to usage history for the SmarTrip card
+3. For each calendar month, export to Excel
+4. Copy and paste rows of data to a master Excel sheet
+4. Station name corrections ("Rosslyn East" to "Rosslyn", "Capitol S" to "Capitol South", etc.)
+5. Change format of all cells in the "Change" and "Balance" columns to numbers rather than currency
+4. If SmartBenefits is used, separate SmartBenefits balance and changes from SmarTrip balance and changes
+5. Create new columns for "Month" and "Weekday"
+4. Convert time into Month and Weekday using Excel format changes
+4. Export whole file as CSV
+
+## Python Steps
+
+1. Convert time strings to timestamps using pandas.to_datetime()
+2. Sort by time ascending (oldest trips to newest)
+3. Group by trips: 
+  1. Iterate through rows; if "Description" is "Entry", then add one to the trip number and append to a list
+  2. Add list to DataFrame as new column "Trip"
+
+
+# Data Analysis
+
+- 688 rows of data. Each row represents one swipe of the SmarTrip card.
+- 282 entries, 179 transfers, 179 exits, 48 reloads/others
+- $606 spent
+
+## Overall Trip Data
+
+- 282 trips
 
 ## Circulator Usage
+
+Filtered the data with [Operator = DC Circulator]:
 
 - 29 boardings (pie chart)
   - 20 Georgetown- Union Station
@@ -13,5 +49,32 @@ Analysis of WMATA performance using Conway's own SmarTrip data
   - 1 Union Station- Navy Yard
   - 1 Woodley Park- Adams Morgan
 - 19 originations, 10 transfers
-- Total cost: $14
-- 
+- $14.00 total cost
+
+## Subway Trips
+
+Filtered the trips by [Operator = Metrorail]:
+- 173 subway trips
+  - 111 entries, 62 transfers from bus-to-subway
+  - Boarded from 32 origination stations [map of frequency]
+  - Traveled to 29 destination stations [map of frequency]
+- $276.00 total cost
+  - Average of $1.60
+  - Max cost of $4.65 (Pentagon to Vienna)
+  - [histogram of cost value_counts]
+- Subway trips by weekday [histogram]
+- Subway trips by month [histogram]
+- Boardings by period of day:
+  - 11 morning (6am-11am)
+  - 56 midday (11am-5pm)
+  - 48 evening (5pm-10pm)
+  - 58 night (10pm-6am)
+- 3 days, 3 hours, and 31 minutes total time
+  - Average duration: 24 minutes, 59 seconds
+  - Shortest trip: 7 minutes (Pentagon to National Airport)
+  - Longest trip: 1 hour, 15 minutes (New York Avenue to Pentagon)
+
+
+
+
+
--- trip-analyzer.py
+++ trip-analyzer.py
@@ -6,21 +6,83 @@ Created on Tue Dec  8 14:41:34 2015
 """
 
 import pandas as pd
-import os
-import time
 import numpy as np
+import datetime
 
 # Read in CSV data
 data = pd.read_csv('trip_data.csv')
 
 # Convert time strings to timestamps
-time_converted = pd.to_datetime(data.Time, format="%m/%d/%Y %H:%M")
-data.Time = time_converted
+time_converted = pd.to_datetime(data.Datetime, format="%m/%d/%Y %H:%M")
+data.Datetime = time_converted
+
+# Group into trips based on "Entry"
+data['Trip'] = 'NaN'
+data.sort(columns = 'Datetime', ascending=True, inplace=True)
+i=0
+trips= []
+for row in data.Description:
+    if row == "Entry":
+        i += 1    
+    trips.append(i)
+data['Trip'] = trips
+
+# Examine trips
+len(data.Trip.value_counts())
+
+# Calculate subway-to-subway trips
+subway_trips=[]
+for i in range(1, len(set(data['Trip']))+1):
+    this_df = data[(data['Trip'] == i) & (data['Operator'] == 'Metrorail') & ( (data['Description'] == 'Entry') | (data['Description'] == 'Transfer') | (data['Description'] == 'Exit'))]
+    if len(this_df) < 2:
+        continue
+    this_trip = {'trip': i, 'entr_trfr':this_df.iloc[0]['Description']}
+    this_trip['entr_trfr'] = this_df.iloc[0]['Description']
+    if this_df.iloc[0]['Description'] == 'Transfer':
+        this_trip['origin'] = this_df.iloc[0]['Exit']
+    else:
+        this_trip['origin'] = this_df.iloc[0]['Entry_Route']
+    this_trip['destination'] = this_df.iloc[-1]['Exit']
+    this_trip['entry_time'] = this_df.iloc[0]['Datetime']
+    this_trip['exit_time'] = this_df.iloc[-1]['Datetime']
+    this_trip['weekday'] = this_df.iloc[0]['Weekday']
+    this_trip['duration'] = this_df.iloc[-1]['Datetime'] - this_df.iloc[0]['Datetime']
+    this_trip['cost'] = this_df.iloc[0]['Balance'] - this_df.iloc[1]['Balance']
+    this_trip['month'] = this_df.iloc[0]['Month']
+    subway_trips.append(this_trip)
+    
+columns = ['trip', 'month', 'weekday', 'entr_trfr', 'origin', 'destination', 'entry_time', 'exit_time', 'duration', 'cost']
+subway_df = pd.DataFrame(subway_trips, columns=columns)
+subway_df.set_index('trip', inplace=True)
+subway_df.drop([269, 188], inplace=True) # Drop erroneous trip duration rows
+
+# Period of day
+pd_list = []
+for i in subway_df.entry_time:
+    if (17 < i.hour < 22):
+        pd_list.append('evening')
+    elif (6 < i.hour < 11):
+        pd_list.append('morning')
+    elif (11 < i.hour < 17):
+        pd_list.append('midday')
+    else:
+        pd_list.append('night')
+subway_df['period'] = pd_list
+
+# Examine subway trips
+subway_df.columns
+sum(subway_df.cost) # Total cost
+len(subway_df) # Number of trips
+len(subway_df.origin.value_counts())
+subway_df.period.value_counts()
+subway_df.duration.describe() # Total time
+subway_df.sort(columns='duration')
+subway_df.sort(columns=['origin', 'destination'])[['origin', 'destination', 'duration']]
+
 
 # Circulator
 circulator = data[(data.Operator=="DC Circulator") & ((data.Description == "Transfer") | (data.Description == "Entry"))]
 len(circulator)
 
-circulator.Change
-
-pd.plot()
+# Metrobus
+data.Trip
