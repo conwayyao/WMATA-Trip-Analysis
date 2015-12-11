08812ad7f287abaa75a8536fdc7fb93cdd39c7b1
Conway Yao <theyaomeister@gmail.com>
2015-12-10 22:22:24 -0500

    Major work on trip analyzer. Bus analysis

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # WMATA Trip Analysis
+
 Analysis of WMATA performance using Conway's own SmarTrip data
 
 # Data Analysis
@@ -17,8 +18,8 @@ Filtered the data with [Operator = DC Circulator]:
 
 - 29 boardings (pie chart)
   - 20 Georgetown- Union Station
-  - 7 Rosslyn- Dupont Circle
-  - 1 Union Station- Navy Yard
+  - 7 Rosslyn- Dupont Circle 
+  - 1 Union Station- Navy Yard 
   - 1 Woodley Park- Adams Morgan
 - 19 originations, 10 transfers
 - $14.00 total cost
@@ -26,22 +27,50 @@ Filtered the data with [Operator = DC Circulator]:
 ## Subway Trips
 
 Filtered the trips by [Operator = Metrorail]:
-- 173 subway trips
-  - 111 entries, 62 transfers from bus-to-subway
-  - Boarded from 32 origination stations [map of frequency]
-  - Traveled to 29 destination stations [map of frequency]
-- $276.00 total cost
-  - Average of $1.60
-  - Max cost of $4.65 (Pentagon to Vienna)
+
+- 170 subway trips
+  - 111 entries, 59 transfers from bus-to-subway
+  - Boarded from 32 origination stations [map of frequency] 
+  - Traveled to 29 destination stations [map of frequency] 
+- $273.10 total cost
+  - Average fare of $1.61
+  - Max fare of $4.65 (Pentagon to Vienna) 
   - [histogram of cost value_counts]
-- Subway trips by weekday [histogram]
+- Subway trips by weekday [histogram] 
 - Subway trips by month [histogram]
-- Boardings by period of day:
+- Boardings by period of day: 
   - 11 morning (6am-11am)
   - 56 midday (11am-5pm)
   - 48 evening (5pm-10pm)
   - 58 night (10pm-6am)
-- 3 days, 3 hours, and 31 minutes total time
-  - Average duration: 24 minutes, 59 seconds
+- 43 peak-fare trips:
+  - $1.81 average fare
+  - 21m 40s average duration
+- 127 off-peak trips
+  - $1.54 average fare
+  - 25m 26s average duration
+- 2 days, 21 hours, 24 minutes total time
+  - Average duration: 24 minutes, 29 seconds
+  - Median duration: 21 minutes
+  - 75% duration: 27 minutes
   - Shortest trip: 7 minutes (Pentagon to National Airport)
   - Longest trip: 1 hour, 15 minutes (New York Avenue to Pentagon)
+
+## Bus trips
+
+Filtered the trips by [Operator = Metrobus, DASH, or FFC]:
+
+- 250 bus trips 
+  - 149 entries, 101 transfers from subway-to-bus or bus-to-bus
+  - Boarded 41 routes [map of frequency] 
+- $304.95 total cost
+  - Average fare of $1.22
+  - Max fare of $7.00 (Greenbelt to BWI) 
+  - [histogram of cost value_counts]
+- Subway trips by weekday [histogram] 
+- Subway trips by month [histogram]
+- Boardings by period of day: 
+  - 28 morning (5am-11am)
+  - 44 midday (11am-5pm)
+  - 54 evening (5pm-10pm)
+  - 124 night (10pm-5am)
--- trip-analyzer.py
+++ trip-analyzer.py
@@ -12,13 +12,13 @@ import datetime
 # Read in CSV data
 data = pd.read_csv('trip_data.csv')
 
-# Convert time strings to timestamps
+## Convert time strings to timestamps
 time_converted = pd.to_datetime(data.Datetime, format="%m/%d/%Y %H:%M")
 data.Datetime = time_converted
 
-# Group into trips based on "Entry"
+## Group into trips based on "Entry"
 data['Trip'] = 'NaN'
-data.sort(columns = 'Datetime', ascending=True, inplace=True)
+data.sort_values(by = 'Datetime', ascending=True, inplace=True)
 i=0
 trips= []
 for row in data.Description:
@@ -27,8 +27,11 @@ for row in data.Description:
     trips.append(i)
 data['Trip'] = trips
 
-# Examine trips
+## Examine trips
 len(data.Trip.value_counts())
+
+## Drop erroneous trip duration rows
+data.drop(data[data.Trip.isin([184, 188, 269])].index, inplace=True) 
 
 # Calculate subway-to-subway trips
 subway_trips=[]
@@ -54,9 +57,8 @@ for i in range(1, len(set(data['Trip']))+1):
 columns = ['trip', 'month', 'weekday', 'entr_trfr', 'origin', 'destination', 'entry_time', 'exit_time', 'duration', 'cost']
 subway_df = pd.DataFrame(subway_trips, columns=columns)
 subway_df.set_index('trip', inplace=True)
-subway_df.drop([269, 188], inplace=True) # Drop erroneous trip duration rows
 
-# Period of day
+## Period of day
 pd_list = []
 for i in subway_df.entry_time:
     if (17 < i.hour < 22):
@@ -69,20 +71,73 @@ for i in subway_df.entry_time:
         pd_list.append('night')
 subway_df['period'] = pd_list
 
-# Examine subway trips
+## On/off peak? 
+weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+weekend = ['Friday', 'Saturday']
+subway_df['peak'] = False
+for i, row in subway_df.iterrows():
+    if ((row['weekday'] in weekday) & (5<(row['entry_time'].hour+row['entry_time'].minute/60.0)<9.5)):
+        subway_df.set_value(i, 'peak', True)
+    elif ((row['weekday'] in weekday) & (15<row['entry_time'].hour<19)):
+        subway_df.set_value(i, 'peak', True)
+    elif ((row['weekday'] in weekend) & (row['entry_time'].hour<4)):
+        subway_df.set_value(i, 'peak', True)
+
+## Examine subway trips
 subway_df.columns
 sum(subway_df.cost) # Total cost
+subway_df.cost.describe() # Cost description
 len(subway_df) # Number of trips
-len(subway_df.origin.value_counts())
-subway_df.period.value_counts()
-subway_df.duration.describe() # Total time
-subway_df.sort(columns='duration')
-subway_df.sort(columns=['origin', 'destination'])[['origin', 'destination', 'duration']]
+subway_df.entr_trfr.value_counts() # Entries/Transfers
+len(subway_df.destination.value_counts()) # Number of stations visited
+subway_df.period.value_counts() # Time of day
+subway_df.duration.sum() # Total time
+subway_df.duration.describe() # Duration description
+subway_df.sort_values(by='duration').tail(5) # Longest trips
+subway_df.sort_values(by=['origin', 'destination'])[['origin', 'destination', 'duration']]
 
+## Examine peak fare trips
+subway_df.peak.value_counts() # Number of peak fare trips
+subway_df.groupby(by='peak').duration.describe() # Peak fare duration
 
 # Circulator
 circulator = data[(data.Operator=="DC Circulator") & ((data.Description == "Transfer") | (data.Description == "Entry"))]
-len(circulator)
+len(circulator) # Total Circulator boardings
+circulator.columns
 
-# Metrobus
-data.Trip
+# Bus trips
+bus_sys= ['Metrobus', 'DASH Bus', 'FFC Bus']
+bus_descrips = ['Entry', 'Transfer']
+bus = data[(data.Operator.isin(bus_sys)) & (data.Description.isin(bus_descrips))]
+
+## Examine bus trips
+bus.columns
+len(bus) # Number of bus trips
+bus.Description.value_counts() # Entry/transfers
+len(bus.Entry_Route.value_counts()) # Bus routes
+bus.Change.value_counts() # Fares
+bus.Weekday.value_counts() # Day of week
+
+# Bus period
+bus_pd_list = []
+for i in bus.Datetime:
+    if (17 < i.hour < 22):
+        bus_pd_list.append('evening')
+    elif (5 < i.hour < 11):
+        bus_pd_list.append('morning')
+    elif (11 < i.hour < 17):
+        bus_pd_list.append('midday')
+    else:
+        bus_pd_list.append('night')
+bus['period'] = bus_pd_list
+bus.period.value_counts()
+
+# Bus duration
+trips_with_dash = data[data.Trip.isin(data[data.Operator=='DASH Bus'].Trip.values)]
+trips_with_dash['duration'] = trips_with_dash['Datetime'].diff()
+multi_trips = trips_with_dash.Trip.value_counts()>1
+multi_trips = multi_trips[multi_trips==True].index
+sort1 = trips_with_dash[trips_with_dash.Trip.isin(multi_trips)]
+sort1[sort1.Description == 'Transfer'][['Trip', 'Operator', 'Description', 'duration']]
+            
+    
